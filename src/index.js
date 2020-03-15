c758ba443f23262103d77130424b51a38d0a9fee
Amnah Khatun <amna.khatun1605@gmail.com>
2020-03-15 13:28:55 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import postReducer from "./reducers/postReducers";
+const store = createStore(postReducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
